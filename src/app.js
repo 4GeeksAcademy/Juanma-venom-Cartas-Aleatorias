e7163a6ccca5844cd6fc2da32689911d6d42074f
PRODY (Juanma) <69425794+Juanma-venom@users.noreply.github.com>
2025-05-28 15:30:17 +0000

    Cartas Aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import "bootstrap";
-import "./style.css";
+const suits = ["♦", "♥", "♠", "♣"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+function generateCard() {
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const valueIndex = Math.floor(Math.random() * values.length);
+  const suit = suits[suitIndex];
+  const value = values[valueIndex];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const card = document.getElementById("card");
+  const topSuit = document.getElementById("topSuit");
+  const bottomSuit = document.getElementById("bottomSuit");
+  const cardValue = document.getElementById("cardValue");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Remplazar clases anteriores con cada actualización de página.
+  card.classList.remove("heart", "diamond", "spade", "club");
+
+  // Asignar clase según el palo
+  switch (suit) {
+    case "♥":
+      card.classList.add("heart");
+      break;
+    case "♦":
+      card.classList.add("diamond");
+      break;
+    case "♠":
+      card.classList.add("spade");
+      break;
+    case "♣":
+      card.classList.add("club");
+      break;
+  }
+
+  // Establecer contenido
+  topSuit.textContent = suit;
+  bottomSuit.textContent = suit;
+  cardValue.textContent = value;
+}
+
+// Generar carta al cargar
+window.onload = () => {
+  generateCard();
+  setInterval(generateCard, 10000); // Cada 10 segundos
 };
